c5662052695b148168a7014e21d6b5c3bf19ac78
Andre Grossi <iamandregrossi@gmail.com>
2023-01-22 22:07:27 -0300

    chore(index): Cleanup index page

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,17 @@
-import './tailwind.css';
+import moon from './assets/icon-moon.svg';
+import logo from './assets/logo.svg';
 
-import { useState } from 'react';
-
-import logo from './logo.svg';
-
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
-    <div className="text-center">
-      <header className="bg-slate-300 h-screen flex flex-col items-center justify-center">
-        <img
-          src={logo}
-          className="h-[40vmin] pointer-events-none animate-spin-slow"
-          alt="logo"
-        />
-        <h1 className="text-3xl font-bold text-blue-600">
-          🚀 Vite + React + Typescript 🤘 & <br />
-          Eslint 🔥+ Prettier
-        </h1>
-
-        <div className="text-md mt-5 mx-3">
-          <button
-            className="bg-blue-300 px-3 py-2 border-2 rounded-lg border-blue-400 "
-            onClick={() => setCount((count) => count + 1)}
-          >
-            🪂 Click me : {count}
-          </button>
-
-          <p className="mt-3">
-            Don&apos;t forget to install Eslint and Prettier in Your Vscode.
-          </p>
-
-          <p>
-            Mess up the code in <code>App.tsx </code> and save the file.
-          </p>
-          <p>
-            <a
-              className="text-cyan-600 font-semibold underline"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-            {' | '}
-            <a
-              className="text-cyan-600 font-semibold underline"
-              href="https://vitejs.dev/guide/features.html"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Vite Docs
-            </a>
-          </p>
-        </div>
-      </header>
+    <div className="bg-white flex flex-col items-center">
+      <div className="max-w-3xl mt-10 w-full">
+        <header className="flex flex-row items-center justify-between">
+          <img src={logo} alt="logo" />
+          <img src={moon} alt="theme" />
+        </header>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
