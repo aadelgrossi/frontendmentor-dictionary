c6495b4397d54408c0b7764c751122590478b27f
Andre Grossi <iamandregrossi@gmail.com>
2023-01-23 21:55:29 -0300

    feat(SearchResult): Create component and render api response

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,19 @@ import { useState } from 'react';
 import moon from './assets/icon-moon.svg';
 import logo from './assets/logo.svg';
 import SearchInput from './components/SearchInput';
+import SearchResult from './components/SearchResult';
 import useDictionarySearch from './services/useDictionarySearch';
 
 const App = () => {
-  const [word, setWord] = useState('hello');
+  const [word, setWord] = useState('keyboard');
 
   const { data } = useDictionarySearch(word);
 
   return (
-    <div className="bg-white flex flex-col items-center mx-6">
+    <div
+      className="flex flex-col items-center mx-6
+      bg-white text-text-200"
+    >
       <div className="max-w-3xl mt-10 w-full">
         <header className="flex flex-row items-center justify-between mb-14">
           <img src={logo} alt="logo" />
@@ -19,7 +23,9 @@ const App = () => {
         </header>
         <SearchInput value={word} onChange={(e) => setWord(e.target.value)} />
 
-        {JSON.stringify(data, null, 2)}
+        {data?.map((result) => {
+          return <SearchResult key={result.word} result={result} />;
+        })}
       </div>
     </div>
   );
---
+++ src/components/SearchResult.tsx
@@ -0,0 +1,105 @@
+import { ReactNode } from 'react';
+
+import link from '../assets/icon-new-window.svg';
+import play from '../assets/icon-play.svg';
+import { DictionaryEntry } from '../services/useDictionarySearch';
+
+interface SearchResultProps {
+  result: DictionaryEntry;
+}
+
+const SectionTitle = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className="flex flex-row items-center gap-3">
+      <span className="italic font-bold text-md">{children}</span>
+      <hr className="w-full divide-zinc-800" />
+    </div>
+  );
+};
+
+const SearchResult = ({ result }: SearchResultProps) => {
+  const { word, phonetic, meanings, sourceUrls } = result;
+
+  return (
+    <div id="container" className="mt-6 flex flex-col">
+      <div id="word" className="flex flex-row justify-between">
+        <div>
+          <h1 className="font-bold text-[32px]">{word}</h1>
+          <p className="text-primary text-md">{phonetic}</p>
+        </div>
+        <button aria-label="play-phonetic" aria-roledescription="Play phonetic">
+          <img alt="play-phonetic" src={play} />
+        </button>
+      </div>
+
+      {meanings.map((meaning) => {
+        const { partOfSpeech, definitions, synonyms } = meaning;
+        return (
+          <div id={`meaning-${partOfSpeech}`} className="mt-8" key={partOfSpeech}>
+            <SectionTitle>{meaning.partOfSpeech}</SectionTitle>
+            <p className="text-gray-500 mt-6 mb-4">Meaning</p>
+
+            {definitions.map((definition, idx) => {
+              return (
+                <div key={idx}>
+                  <ul className="list-disc marker:text-primary text-[15px] list-inside leading-6">
+                    <li className="mb-3" key={idx}>
+                      {definition.definition}
+                    </li>
+                  </ul>
+
+                  {definition.example && (
+                    <p className="ml-5 text-accent-500">
+                      &quot;{definition.example}&quot;
+                    </p>
+                  )}
+                </div>
+              );
+            })}
+
+            {synonyms.length > 0 && (
+              <div className="flex flex-row">
+                <p className="text-gray-500 mr-6">Synonyms</p>
+
+                <div className="flex flex-row gap-2 flex-wrap">
+                  {synonyms.map((synonym, idx) => {
+                    return (
+                      <a
+                        key={idx}
+                        className="font-semibold text-primary hover:underline cursor-pointer"
+                      >
+                        {synonym}
+                      </a>
+                    );
+                  })}
+                </div>
+              </div>
+            )}
+          </div>
+        );
+      })}
+
+      <hr className="my-6" />
+
+      <div className="flex flex-col gap-1">
+        <p className="underline text-gray-500">Source</p>
+        {sourceUrls.map((url) => {
+          return (
+            <a
+              key={url}
+              href={url}
+              target="_blank"
+              className="flex flex-row gap-2 underline"
+              rel="noreferrer"
+            >
+              {url}
+              <img alt="source" src={link} />
+            </a>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default SearchResult;
